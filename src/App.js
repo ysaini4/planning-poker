41d42dc687921a75299b084f440a481f5b6650ec
yogesh.saini <yogesh.saini@clover.com>
2023-07-03 14:18:46 +0530

    s

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {SocketContext} from './Socketcontext';
 //const server = 'https://fast-depths-40126.herokuapp.com';
 const server = 'https://poker-server-vkeg.onrender.com';
 const server_local = 'http://localhost:3001';
-const socket = io(server_local, {  autoConnect: true, transports: ['websocket'] }); // connect to the socket.io server
+const socket = io(server, {  autoConnect: true, transports: ['websocket'] }); // connect to the socket.io server
 function App() {
   
   return (
